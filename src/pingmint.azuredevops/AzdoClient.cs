2f37bc074044b96741c81321afc0c68b98ab7876
Joe Gallagher <joegaggler@gmail.com>
2023-11-02 16:32:10 -0400

    drop alpha package version

--- src/pingmint.azuredevops/AzdoClient.cs
+++ src/pingmint.azuredevops/AzdoClient.cs
@@ -108,8 +108,7 @@ public partial class AzdoClient
 
         var response = await httpClient.SendAsync(message);
         return await response.Content.ReadAsStringAsync();
-    }
-    
+    }    
 
     public async Task<String> GetStringAsync(Uri uri)
     {
